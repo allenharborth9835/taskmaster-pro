86907e69e6ae567a0a2e3bb17032c1e7d031b0ab
Harborth, Allen <allenharborth9835@gmail.com>
2021-07-18 22:04:42 -0500

    add drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,6 +128,49 @@ $(".list-group").on("blur","input[type='text']",function(){
   $(this).replaceWith(taskSpan);
 })
 
+$(".card .list-group").sortable({
+
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance:"pointer",
+  helper: "clone",
+  update: function(event){
+    var tempArr = [];
+    var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      //add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date:date
+      });
+    //trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    ui.draggable.remove();
+  },
+  over:function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
